700209c02535084aa0a6b7cf22b21e7fd8bfee33
marossmrek <marossmrek@gmail.com>
2017-11-13 16:09:06 +0100

    Add message snackbar

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import SelectField from 'material-ui/SelectField';
 import MenuItem from 'material-ui/MenuItem';
 import Checkbox from 'material-ui/Checkbox';
 import RaisedButton from 'material-ui/RaisedButton';
+import Snackbar from 'material-ui/Snackbar';
 
 import Form from './Service/form-service';
 
@@ -26,7 +27,11 @@ class App extends React.Component {
                 files: []
             },
             formErrors: {},
-            isLogin: false
+            isLogin: false,
+            snackBar: {
+                isOpen: false,
+                msg: ""
+            }
         }
     }
 
@@ -51,6 +56,10 @@ class App extends React.Component {
             formData: {
                 ...prevState.formData,
                 [formDataType]: formDataValue
+            },
+            snackBar: {
+                isOpen: false,
+                msg: ""
             }
         }));
     }
@@ -66,7 +75,6 @@ class App extends React.Component {
     async sendFormData() {
         var result = await Form.submit(this.state.formData);
         if (result) {
-            console.log(result);
             if (result.status === 200 && result.data) {
                 this.setState({
                     formErrors: {
@@ -74,7 +82,26 @@ class App extends React.Component {
                     }
                 });
             } else if (result.status === 204) {
-                console.log("Success");
+                this.setState({
+                    formData: {
+                        name: "",
+                        email: "",
+                        numberOfKids: null,
+                        accepted: false,
+                        files: []
+                    },
+                    snackBar: {
+                        isOpen: true,
+                        msg: "Send data successfully"
+                    }
+                })
+            } else if (result.status === 500) {
+                this.setState({
+                    snackBar: {
+                        isOpen: true,
+                        msg: "Something wrong, please try late"
+                    }
+                })
             }
         }
     }
@@ -83,7 +110,7 @@ class App extends React.Component {
         const formErrors = {};
 
         const formData = this.state.formData;
-        console.log(formData.files);
+
         const reqExEmail = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
         formErrors.name = formData.name.trim().length < 5 && "Name needs to be at least 5 characters long";
@@ -110,75 +137,85 @@ class App extends React.Component {
         const {formData, formErrors} = this.state;
         return (
             <MuiThemeProvider>
-                <Paper className="form-container" zDepth={3} rounded={false}>
-                    <TextField onChange={this.handleTextChange.bind(this)}
-                               name="name"
-                               fullWidth={true}
-                               value={formData.name}
-                               hintText="Name"
-                               errorText={formErrors.name}
-                               errorStyle={{textAlign: "left"}}/>
-                    <TextField onChange={this.handleTextChange.bind(this)}
-                               name="email"
-                               fullWidth={true}
-                               value={formData.email}
-                               hintText="Email"
-                               errorText={formErrors.email}
-                               errorStyle={{textAlign: "left"}}/>
-                    <SelectField
-                        onChange={this.handleSelectChange.bind(this)}
-                        name="numberOfKids"
-                        fullWidth={true}
-                        value={formData.numberOfKids}
-                        hintText="Number of kids"
-                        errorText={formErrors.numberOfKids}
-                        errorStyle={{textAlign: "left"}}
-                        selectedMenuItemStyle={{color: "#00BCD4"}}
-                        className="form-type"
-                    >
-                        <MenuItem key={1} value={1} primaryText="One"/>
-                        <MenuItem key={2} value={2} primaryText="Two"/>
-                        <MenuItem key={3} value={3} primaryText="Three"/>
-                        <MenuItem key={4} value={4} primaryText="Four"/>
-                        <MenuItem key={5} value={5} primaryText="Five"/>
-                    </SelectField>
-                    <div className="form-type">
-                        <Checkbox
-                            name="test"
-                            label="I accept everything"
-                            checked={formData.accepted}
-                            onCheck={this.updateCheck.bind(this)}
-                        />
-                        <div className="error">{formErrors.accepted}</div>
-                    </div>
-                    <div>
-                        <Dropzone multiple={true} onDrop={this.onDrop.bind(this)} accept="image/png"
-                                  className="dropzone">
-                            {({acceptedFiles, rejectedFiles}) => {
-                                return acceptedFiles.length || rejectedFiles.length
-                                    ? `Accepted ${acceptedFiles.length}, rejected ${rejectedFiles.length} files`
-                                    : "Try dropping some png";
-                            }}
-                        </Dropzone>
-                        <div className="error">{formErrors.files}</div>
-                    </div>
-                    <div>
-                        {!this.state.isLogin ?
-                            <GoogleLogin
-                                clientId="333261271673-dpbki5e39148gtpaqisni0sv8qttejrt.apps.googleusercontent.com"
-                                buttonText="Login with Google"
-                                className="loginBtn loginBtn-google"
-                                onSuccess={() => this.setState({
-                                    isLogin: true
-                                })}
+                <div>
+                    <Paper className="form-container" zDepth={3} rounded={false}>
+                        <TextField onChange={this.handleTextChange.bind(this)}
+                                   name="name"
+                                   fullWidth={true}
+                                   value={formData.name}
+                                   hintText="Name"
+                                   errorText={formErrors.name}
+                                   errorStyle={{textAlign: "left"}}/>
+                        <TextField onChange={this.handleTextChange.bind(this)}
+                                   name="email"
+                                   fullWidth={true}
+                                   value={formData.email}
+                                   hintText="Email"
+                                   errorText={formErrors.email}
+                                   errorStyle={{textAlign: "left"}}/>
+                        <SelectField
+                            onChange={this.handleSelectChange.bind(this)}
+                            name="numberOfKids"
+                            fullWidth={true}
+                            value={formData.numberOfKids}
+                            hintText="Number of kids"
+                            errorText={formErrors.numberOfKids}
+                            errorStyle={{textAlign: "left"}}
+                            selectedMenuItemStyle={{color: "#00BCD4"}}
+                            className="form-type"
+                        >
+                            <MenuItem key={1} value={1} primaryText="One"/>
+                            <MenuItem key={2} value={2} primaryText="Two"/>
+                            <MenuItem key={3} value={3} primaryText="Three"/>
+                            <MenuItem key={4} value={4} primaryText="Four"/>
+                            <MenuItem key={5} value={5} primaryText="Five"/>
+                        </SelectField>
+                        <div className="form-type">
+                            <Checkbox
+                                name="test"
+                                label="I accept everything"
+                                checked={formData.accepted}
+                                onCheck={this.updateCheck.bind(this)}
                             />
-                          :
-                            <RaisedButton label="Submit" primary={true}
-                                          onClick={this.handleSubmit.bind(this)}/>
-                        }
-
-                    </div>
-                </Paper>
+                            <div className="error">{formErrors.accepted}</div>
+                        </div>
+                        <div>
+                            <Dropzone multiple={true} onDrop={this.onDrop.bind(this)} accept="image/png"
+                                      className="dropzone">
+                                {({acceptedFiles, rejectedFiles}) => {
+                                    return acceptedFiles.length || rejectedFiles.length
+                                        ? `Accepted ${acceptedFiles.length}, rejected ${rejectedFiles.length} files`
+                                        : "Try dropping some png";
+                                }}
+                            </Dropzone>
+                            <div className="error">{formErrors.files}</div>
+                        </div>
+                        <div>
+                            {
+                                !this.state.isLogin ?
+                                    <GoogleLogin
+                                        clientId="333261271673-dpbki5e39148gtpaqisni0sv8qttejrt.apps.googleusercontent.com"
+                                        buttonText="Login with Google"
+                                        className="loginBtn loginBtn-google"
+                                        onSuccess={() => this.setState({
+                                            isLogin: true,
+                                            snackBar: {
+                                                isOpen: true,
+                                                msg: "Google authetification success, please submit form"
+                                            }
+                                        })}
+                                    /> :
+                                    <RaisedButton label="Submit" primary={true} onClick={this.handleSubmit.bind(this)}/>
+                            }
+                        </div>
+                    </Paper>
+                    <Snackbar
+                        open={this.state.snackBar.isOpen}
+                        message={this.state.snackBar.msg}
+                        autoHideDuration={50000}
+                        className="snackbar"
+                    />
+                </div>
             </MuiThemeProvider>
         );
     }
